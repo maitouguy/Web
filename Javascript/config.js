64080ea90a8e924b0bb4b4fe3eb5e714a3f06321
Yahya Ait ahmed <Yahyaaitahmed20@gmail.com>
2024-07-10 19:22:29 +0100

    just did some weird shit : Version 0.2

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,3 +1,5 @@
 console.log("This is an algorithm to know which tiktoks to display (fbi i'm joking pls don't arrest me)")
 var videoToShow = "fuck you lil";
 console.log(videoToShow + "fuck this shit");
+var veryComplicatedIncredibleAlgorithm = "if user likes cat videos, show him more catVideos"
+veryComplicatedIncredibleAlgorithm.Apply();
