574f6d4ac5316e84eec9612e4a97c22ce1b96b3e
Yahya Ait ahmed <Yahyaaitahmed20@gmail.com>
2024-07-10 19:43:59 +0100

    just added a single fking line of code : Version 0.3

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,3 +1,4 @@
 console.log("This is an algorithm to know which tiktoks to display (fbi i'm joking pls don't arrest me)")
 var videoToShow = "fuck you lil";
 console.log(videoToShow + "fuck this shit");
+console.log("alias tutorial")
