c854000da519838957874c3a7dc7627c1174d096
Yahya Ait ahmed <Yahyaaitahmed20@gmail.com>
2024-07-10 19:36:12 +0100

    interesting

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,5 +1,3 @@
 console.log("This is an algorithm to know which tiktoks to display (fbi i'm joking pls don't arrest me)")
 var videoToShow = "fuck you lil";
 console.log(videoToShow + "fuck this shit");
-var veryComplicatedIncredibleAlgorithm = "if user likes cat videos, show him more catVideos"
-veryComplicatedIncredibleAlgorithm.Apply();
